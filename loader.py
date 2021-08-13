f8062a2b8954252e6e26771db658455c05d43baa
TimNekk <herew26@gmail.com>
2021-08-13 04:19:11 +0300

    run_blocking_io function added

--- loader.py
+++ loader.py
@@ -1,7 +1,21 @@
+import asyncio
+import concurrent.futures
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
+
+
+async def run_blocking_io(func, *args):
+    loop = asyncio.get_running_loop()
+
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(
+            pool, func, *args
+        )
+
+    return result
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
