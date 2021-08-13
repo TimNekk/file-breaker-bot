7ce4a329b34ac50b049ca3f7730bfca6b70a5ecf
TimNekk <herew26@gmail.com>
2021-08-13 04:19:35 +0300

    start text changed

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,9 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer("""
+Привет, я <b>Разрушитель файлов</b>! 💣
+
+Отправь мне любой файл <i>(фото, видео, документ, песню и. д.)</i>
+И я сделаю так, чтобы его невозможно было открыть!
+""")
